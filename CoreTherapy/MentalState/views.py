1bc034c4e3e19d7e48703b58228cadaac9297b6b
Mike Okoth <65109328+myk4040okothogodo@users.noreply.github.com>
2022-04-28 15:46:31 +0300

    Update views.py

--- CoreTherapy/MentalState/views.py
+++ CoreTherapy/MentalState/views.py
@@ -9,13 +9,13 @@ from .serializer import mentalStateSerializer
 class DefaultsMixin(object):
     """Default settings for view authentication, permissions,
     filtering and pagination."""
-    authentication_classes = (
-        authentication.BasicAuthentication,
-        authentication.TokenAuthentication,
-    )
-    permission_classes = (
-        permissions.IsAuthenticated,
-    )
+    #authentication_classes = (
+    #    authentication.BasicAuthentication,
+    #    authentication.TokenAuthentication,
+    #)
+    #permission_classes = (
+    #    permissions.IsAuthenticated,
+    #)
     paginate_by = 25
     paginate_by_param = 'page_size'
     max_paginate_by = 100
