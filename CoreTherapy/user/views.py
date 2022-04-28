6abca5cd90e55527e91731abf9f4e6e0222728eb
Mike Okoth <65109328+myk4040okothogodo@users.noreply.github.com>
2022-04-28 15:50:42 +0300

    Update views.py

--- CoreTherapy/user/views.py
+++ CoreTherapy/user/views.py
@@ -8,13 +8,13 @@ User = get_user_model()
 
 class DefaultsMixin(object):
     """Default settings for view authentication, permissions, filtering and pagination."""
-    authentication_classes = (
-        authentication.BasicAuthentication,
-        authentication.TokenAuthentication,
-            )
-    permission_classes = (
-        permissions.IsAuthenticated,
-            )
+    #authentication_classes = (
+        #authentication.BasicAuthentication,
+        #authentication.TokenAuthentication,
+          #  )
+    #permission_classes = (
+    #    permissions.IsAuthenticated,
+    #        )
     paginate_by = 25
     paginate_by_param = 'page_size'
     max_paginate_by = 100
